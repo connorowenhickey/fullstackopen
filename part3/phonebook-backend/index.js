fa7c10f2457331c4b3ee1eda23a4b253ca99f81b
connorowenhickey <185983925+connorowenhickey@users.noreply.github.com>
2026-08-04 19:46:06 +0100

    Added port connection/CORS from backend to frontend

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(express.static('dist'))
 
 morgan.token('body', request => {
   return JSON.stringify(request.body)
